c07d80439a3a1e8f6f6e70adb9eae6c093b337c0
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-07-27 20:31:14 +0200

    window with vulkan instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,98 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use anyhow::{anyhow, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::{Window, WindowBuilder};
+
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+const VALIDATION_LAYER: vk::ExtensionName = vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    // Window
+    let event_loop = EventLoop::new()?;
+    let window = WindowBuilder::new()
+        .with_title("Vulkan Playground (Rust)")
+        .with_inner_size(LogicalSize::new(1024, 768))
+        .build(&event_loop)?;
+
+    let mut app = unsafe { App::create(&window)? };
+    event_loop.run(|event, elwt| {
+        match event {
+            Event::WindowEvent {event, .. } => match event {
+                WindowEvent::RedrawRequested if !elwt.exiting() => unsafe { app.render(&window).unwrap() },
+                WindowEvent::CloseRequested => {
+                    elwt.exit();
+                    unsafe { app.destroy() };
+                }
+                _ => {}
+            }
+            _ => {}
+        }
+    })?;
+
+    Ok(())
 }
+
+struct App {
+    entry: Entry,
+    instance: Instance
+}
+
+impl App {
+    unsafe fn create(window: &Window) -> Result<Self> { unsafe {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
+    }}
+
+    unsafe fn render (&mut self, window: &Window) -> Result<()> {
+        Ok(())
+    }
+
+    unsafe fn destroy(&mut self) { unsafe {
+        self.instance.destroy_instance(None);
+    }}
+}
+
+struct AppData {}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> { unsafe {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Playground\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let available_layers = entry
+        .enumerate_instance_layer_properties()?
+        .iter()
+        .map(|l| l.layer_name)
+        .collect::<HashSet<_>>();
+
+    if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
+        return Err(anyhow!("Validation layer requested but not supported"));
+    }
+
+    let layers = if VALIDATION_ENABLED { vec![VALIDATION_LAYER.as_ptr()] } else { vec![] };
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
+}}
