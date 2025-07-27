d2ade0b62da2e16b3a8ee0611a6404e3536fe8a5
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-07-27 21:43:14 +0200

    add validation logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::HashSet;
+use std::ffi::{c_void, CStr};
 use anyhow::{anyhow, Result};
 use log::*;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::prelude::v1_0::*;
+use vulkanalia::vk::{DebugUtilsMessageSeverityFlagsEXT, ExtDebugUtilsExtension};
 use vulkanalia::window as vk_window;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
@@ -40,17 +42,20 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Clone, Debug)]
 struct App {
     entry: Entry,
-    instance: Instance
+    instance: Instance,
+    data: AppData,
 }
 
 impl App {
     unsafe fn create(window: &Window) -> Result<Self> { unsafe {
         let loader = LibloadingLoader::new(LIBRARY)?;
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
-        let instance = create_instance(window, &entry)?;
-        Ok(Self { entry, instance })
+        let mut data = AppData::default();
+        let instance = create_instance(window, &entry, &mut data)?;
+        Ok(Self { entry, instance, data })
     }}
 
     unsafe fn render (&mut self, window: &Window) -> Result<()> {
@@ -58,13 +63,23 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) { unsafe {
+        if VALIDATION_ENABLED {
+            self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
+        }
         self.instance.destroy_instance(None);
     }}
 }
 
-struct AppData {}
+#[derive(Clone, Debug, Default)]
+struct AppData {
+    messenger: vk::DebugUtilsMessengerEXT,
+}
 
-unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> { unsafe {
+unsafe fn create_instance(
+    window: &Window,
+    entry: &Entry,
+    data: &mut AppData
+) -> Result<Instance> { unsafe {
     let application_info = vk::ApplicationInfo::builder()
         .application_name(b"Vulkan Playground\0")
         .application_version(vk::make_version(1, 0, 0))
@@ -84,15 +99,58 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> { 
 
     let layers = if VALIDATION_ENABLED { vec![VALIDATION_LAYER.as_ptr()] } else { vec![] };
 
-    let extensions = vk_window::get_required_instance_extensions(window)
+    let mut extensions = vk_window::get_required_instance_extensions(window)
         .iter()
         .map(|e| e.as_ptr())
         .collect::<Vec<_>>();
 
-    let info = vk::InstanceCreateInfo::builder()
+    if VALIDATION_ENABLED {
+        extensions.push(vk::EXT_DEBUG_UTILS_EXTENSION.name.as_ptr());
+    }
+
+    let mut info = vk::InstanceCreateInfo::builder()
         .application_info(&application_info)
         .enabled_layer_names(&layers)
         .enabled_extension_names(&extensions);
 
-    Ok(entry.create_instance(&info, None)?)
+    let mut debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+        .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+        .message_type(vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+            | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+            | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE)
+        .user_callback(Some(debug_callback));
+
+    if VALIDATION_ENABLED {
+        info = info.push_next(&mut debug_info);
+    }
+
+    let instance = entry.create_instance(&info, None)?;
+
+    if VALIDATION_ENABLED {
+        data.messenger = instance.create_debug_utils_messenger_ext(&debug_info, None)?;
+    }
+
+    Ok(instance)
 }}
+
+extern "system" fn debug_callback(
+    severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    type_: vk::DebugUtilsMessageTypeFlagsEXT,
+    data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _: *mut c_void
+) -> vk::Bool32 {
+    let data = unsafe { *data };
+    let message = unsafe { CStr::from_ptr(data.message) }.to_string_lossy();
+
+    if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+        error!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+        warn!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO {
+        debug!("({:?}) {}", type_, message);
+    } else {
+        trace!("({:?}) {}", type_, message);
+    }
+
+    vk::FALSE
+}
