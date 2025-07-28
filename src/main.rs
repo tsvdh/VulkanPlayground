b920d4a55e4eb65dfa76c58ea8ba0dcdb4d1ad81
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-07-28 12:11:38 +0200

    device creation

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::collections::HashSet;
 use std::ffi::{c_void, CStr};
 use anyhow::{anyhow, Result};
 use log::*;
+use thiserror::Error;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::prelude::v1_0::*;
-use vulkanalia::vk::{DebugUtilsMessageSeverityFlagsEXT, ExtDebugUtilsExtension};
+use vulkanalia::vk::{ExtDebugUtilsExtension};
 use vulkanalia::window as vk_window;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
@@ -47,6 +48,7 @@ struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device: Device,
 }
 
 impl App {
@@ -55,7 +57,10 @@ impl App {
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
-        Ok(Self { entry, instance, data })
+        pick_physical_device(&instance, &mut data)?;
+        let device = create_logical_device(&entry, &instance, &mut data)?;
+
+        Ok(Self { entry, instance, data, device })
     }}
 
     unsafe fn render (&mut self, window: &Window) -> Result<()> {
@@ -63,6 +68,7 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) { unsafe {
+        self.device.destroy_device(None);
         if VALIDATION_ENABLED {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
@@ -73,13 +79,11 @@ impl App {
 #[derive(Clone, Debug, Default)]
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
+    physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
 }
 
-unsafe fn create_instance(
-    window: &Window,
-    entry: &Entry,
-    data: &mut AppData
-) -> Result<Instance> { unsafe {
+unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> { unsafe {
     let application_info = vk::ApplicationInfo::builder()
         .application_name(b"Vulkan Playground\0")
         .application_version(vk::make_version(1, 0, 0))
@@ -154,3 +158,85 @@ extern "system" fn debug_callback(
 
     vk::FALSE
 }
+
+#[derive(Debug, Error)]
+#[error("Missing {0}.")]
+struct SuitabilityError(&'static str);
+
+unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> { unsafe {
+    for physical_device in instance.enumerate_physical_devices()? {
+        let properties = instance.get_physical_device_properties(physical_device);
+        if let Err(error) = check_physical_device(instance, data, physical_device) {
+            warn!("Skipping physical device (`{}`): {}", properties.device_name, error)
+        } else {
+            info!("Selected physical device (`{}`)", properties.device_name);
+            data.physical_device = physical_device;
+            return Ok(());
+        }
+    }
+
+    Err(anyhow!("Failed to find suitable physical device"))
+}}
+
+unsafe fn check_physical_device(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice) -> Result<()> { unsafe {
+    let properties = instance.get_physical_device_properties(physical_device);
+    if properties.device_type != vk::PhysicalDeviceType::DISCRETE_GPU {
+        return Err(anyhow!(SuitabilityError("discrete GPU")));
+    }
+    // let features = instance.get_physical_device_features(physical_device);
+
+    QueueFamilyIndices::get(instance, data, physical_device)?;
+
+    Ok(())
+}}
+
+#[derive(Copy, Clone, Debug)]
+struct QueueFamilyIndices {
+    graphics: u32,
+}
+
+impl QueueFamilyIndices {
+    unsafe fn get(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice, ) -> Result<Self> { unsafe {
+        let properties = instance.get_physical_device_queue_family_properties(physical_device);
+
+        let graphics_index = properties
+            .iter()
+            .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
+            .map(|i| {i as u32});
+
+
+        if let Some(graphics_index) = graphics_index {
+            Ok(Self { graphics: graphics_index })
+        } else {
+            Err(anyhow!(SuitabilityError("required queue families")))
+        }
+    }
+}}
+
+unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut AppData) -> Result<Device> { unsafe {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    // only out-of-date versions use device layers
+    let layers = if VALIDATION_ENABLED { vec![VALIDATION_LAYER.as_ptr()] } else { vec![] };
+
+    let extensions = vec![];
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
+}}
