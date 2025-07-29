2a1957f9ecd4011d22fad60dc2a609a83d8cbd05
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-07-29 15:50:56 +0200

    add surface presenting and configure swapchain

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use log::*;
 use thiserror::Error;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::prelude::v1_0::*;
-use vulkanalia::vk::{ExtDebugUtilsExtension};
+use vulkanalia::vk::{ExtDebugUtilsExtension, KhrSurfaceExtension, KhrSwapchainExtension};
 use vulkanalia::window as vk_window;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
@@ -14,6 +14,7 @@ use winit::window::{Window, WindowBuilder};
 
 const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
 const VALIDATION_LAYER: vk::ExtensionName = vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+const DEVICE_EXTENSIONS: &[vk::ExtensionName] = &[vk::KHR_SWAPCHAIN_EXTENSION.name];
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -51,14 +52,34 @@ struct App {
     device: Device,
 }
 
+#[derive(Clone, Debug, Default)]
+struct AppData {
+    messenger: vk::DebugUtilsMessengerEXT,
+    physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
+    surface: vk::SurfaceKHR,
+    present_queue: vk::Queue,
+    swapchain_format: vk::Format,
+    swapchain_extent: vk::Extent2D,
+    swapchain: vk::SwapchainKHR,
+    swapchain_images: Vec<vk::Image>,
+    swapchain_image_views: Vec<vk::ImageView>,
+}
+
 impl App {
     unsafe fn create(window: &Window) -> Result<Self> { unsafe {
         let loader = LibloadingLoader::new(LIBRARY)?;
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
+
+        data.surface = vk_window::create_surface(&instance, window, window)?;
+
         pick_physical_device(&instance, &mut data)?;
         let device = create_logical_device(&entry, &instance, &mut data)?;
+
+        create_swapchain(window, &instance, &device, &mut data)?;
+        create_swapchain_image_views(&device, &mut data)?;
 
         Ok(Self { entry, instance, data, device })
     }}
@@ -68,19 +89,17 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) { unsafe {
+        self.data.swapchain_image_views.iter().for_each(|i| {
+            self.device.destroy_image_view(*i, None);
+        });
+        self.device.destroy_swapchain_khr(self.data.swapchain, None);
         self.device.destroy_device(None);
         if VALIDATION_ENABLED {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
+        self.instance.destroy_surface_khr(self.data.surface, None);
         self.instance.destroy_instance(None);
     }}
-}
-
-#[derive(Clone, Debug, Default)]
-struct AppData {
-    messenger: vk::DebugUtilsMessengerEXT,
-    physical_device: vk::PhysicalDevice,
-    graphics_queue: vk::Queue,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> { unsafe {
@@ -104,8 +123,7 @@ unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) ->
     let layers = if VALIDATION_ENABLED { vec![VALIDATION_LAYER.as_ptr()] } else { vec![] };
 
     let mut extensions = vk_window::get_required_instance_extensions(window)
-        .iter()
-        .map(|e| e.as_ptr())
+        .iter().map(|e| e.as_ptr())
         .collect::<Vec<_>>();
 
     if VALIDATION_ENABLED {
@@ -186,27 +204,64 @@ unsafe fn check_physical_device(instance: &Instance, data: &AppData, physical_de
     // let features = instance.get_physical_device_features(physical_device);
 
     QueueFamilyIndices::get(instance, data, physical_device)?;
+    check_physical_device_extensions(&instance, physical_device)?;
+
+    let support = SwapchainSupport::get(&instance, &data, physical_device)?;
+    if support.formats.is_empty() || support.present_modes.is_empty() {
+        return Err(anyhow!(SuitabilityError("swapchain support")));
+    }
 
     Ok(())
+}}
+
+unsafe fn check_physical_device_extensions(instance: &Instance, physical_device: vk::PhysicalDevice) -> Result<()> { unsafe {
+    let extensions = instance.enumerate_device_extension_properties(physical_device, None)?
+        .iter().map(|e| e.extension_name)
+        .collect::<HashSet<_>>();
+
+    if DEVICE_EXTENSIONS.iter().all(|e| extensions.contains(e)) {
+        Ok(())
+    } else {
+        Err(anyhow!(SuitabilityError("required device extension(s)")))
+    }
 }}
 
 #[derive(Copy, Clone, Debug)]
 struct QueueFamilyIndices {
     graphics: u32,
+    present: u32,
 }
 
 impl QueueFamilyIndices {
     unsafe fn get(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice, ) -> Result<Self> { unsafe {
         let properties = instance.get_physical_device_queue_family_properties(physical_device);
 
-        let graphics_index = properties
-            .iter()
-            .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
-            .map(|i| {i as u32});
+        let mut graphics_index = None;
+        let mut present_index = None;
+        let mut graphics_present_same_queue = false;
+        for (index, properties) in properties.iter().enumerate() {
+            let support_graphics = properties.queue_flags.contains(vk::QueueFlags::GRAPHICS);
+            let support_present = instance.get_physical_device_surface_support_khr(physical_device, index as u32, data.surface)?;
 
+            if support_graphics {
+                graphics_index = Some(index as u32);
+            }
+            if support_present {
+                present_index = Some(index as u32);
+            }
 
-        if let Some(graphics_index) = graphics_index {
-            Ok(Self { graphics: graphics_index })
+            if support_graphics && support_present {
+                graphics_present_same_queue = true;
+                break;
+            }
+        }
+
+        if !graphics_present_same_queue {
+            warn!("Graphics and present queue families are different. Potential performance decrease.")
+        }
+
+        if let (Some(graphics_index), Some(present_index)) = (graphics_index, present_index) {
+            Ok(Self { graphics: graphics_index, present: present_index })
         } else {
             Err(anyhow!(SuitabilityError("required queue families")))
         }
@@ -216,27 +271,155 @@ impl QueueFamilyIndices {
 unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut AppData) -> Result<Device> { unsafe {
     let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
 
+    let unique_indices = HashSet::from([indices.graphics, indices.present]);
+
     let queue_priorities = &[1.0];
-    let queue_info = vk::DeviceQueueCreateInfo::builder()
-        .queue_family_index(indices.graphics)
-        .queue_priorities(queue_priorities);
+    let queue_infos = unique_indices.iter()
+        .map(|i| {
+            vk::DeviceQueueCreateInfo::builder()
+                .queue_family_index(*i)
+                .queue_priorities(queue_priorities)
+        })
+        .collect::<Vec<_>>();
 
     // only out-of-date versions use device layers
     let layers = if VALIDATION_ENABLED { vec![VALIDATION_LAYER.as_ptr()] } else { vec![] };
 
-    let extensions = vec![];
+    let extensions = DEVICE_EXTENSIONS.iter()
+        .map(|n| n.as_ptr())
+        .collect::<Vec<_>>();
 
     let features = vk::PhysicalDeviceFeatures::builder();
 
-    let queue_infos = &[queue_info];
     let info = vk::DeviceCreateInfo::builder()
-        .queue_create_infos(queue_infos)
+        .queue_create_infos(&queue_infos)
         .enabled_layer_names(&layers)
         .enabled_extension_names(&extensions)
         .enabled_features(&features);
 
     let device = instance.create_device(data.physical_device, &info, None)?;
     data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+    data.present_queue = device.get_device_queue(indices.present, 0);
 
     Ok(device)
+}}
+
+#[derive(Clone, Debug)]
+struct SwapchainSupport {
+    capabilities: vk::SurfaceCapabilitiesKHR,
+    formats: Vec<vk::SurfaceFormatKHR>,
+    present_modes: Vec<vk::PresentModeKHR>,
+}
+
+impl SwapchainSupport {
+    unsafe fn get(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice) -> Result<Self> { unsafe {
+        Ok(Self {
+            capabilities: instance.get_physical_device_surface_capabilities_khr(physical_device, data.surface)?,
+            formats: instance.get_physical_device_surface_formats_khr(physical_device, data.surface)?,
+            present_modes: instance.get_physical_device_surface_present_modes_khr(physical_device, data.surface)?,
+        })
+    }}
+}
+
+fn get_swapchain_surface_format(formats: &[vk::SurfaceFormatKHR]) -> vk::SurfaceFormatKHR {
+    formats.iter().cloned()
+        .find(|f| {
+        f.format == vk::Format::B8G8R8A8_SRGB && f.color_space == vk::ColorSpaceKHR::SRGB_NONLINEAR
+        }).unwrap_or(formats[0])
+}
+
+fn get_swapchain_present_mode(present_modes: &[vk::PresentModeKHR]) -> vk::PresentModeKHR {
+    present_modes.iter().cloned()
+        .find(|m| {*m == vk::PresentModeKHR::MAILBOX})
+        .unwrap_or(vk::PresentModeKHR::FIFO)
+}
+
+fn get_swapchain_extent(window: &Window, capabilities: vk::SurfaceCapabilitiesKHR) -> vk::Extent2D {
+    if capabilities.current_extent.width != u32::MAX {
+        capabilities.current_extent
+    } else {
+        vk::Extent2D::builder()
+            .width(window.inner_size().width.clamp(capabilities.min_image_extent.width,
+                                                   capabilities.max_image_extent.width))
+            .height(window.inner_size().height.clamp(capabilities.min_image_extent.height,
+                                                     capabilities.max_image_extent.height))
+            .build()
+    }
+}
+
+unsafe fn create_swapchain(window: &Window, instance: &Instance, device: &Device, data: &mut AppData) -> Result<()> { unsafe {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+    let support = SwapchainSupport::get(instance, data, data.physical_device)?;
+
+    let surface_format = get_swapchain_surface_format(&support.formats);
+    let present_mode = get_swapchain_present_mode(&support.present_modes);
+    let extent = get_swapchain_extent(window, support.capabilities);
+
+    let mut image_count = support.capabilities.min_image_count + 1;
+    let max_image_count = support.capabilities.max_image_count;
+    if max_image_count != 0 && image_count > max_image_count {
+        image_count = max_image_count;
+    }
+
+    let mut queue_family_indices = vec![];
+    let image_sharing_mode = if indices.graphics != indices.present {
+        queue_family_indices.append(&mut vec![indices.graphics, indices.present]);
+        vk::SharingMode::CONCURRENT
+    } else {
+        vk::SharingMode::EXCLUSIVE
+    };
+
+    let info = vk::SwapchainCreateInfoKHR::builder()
+        .surface(data.surface)
+        .min_image_count(image_count)
+        .image_format(surface_format.format)
+        .image_color_space(surface_format.color_space)
+        .image_extent(extent)
+        .image_array_layers(1)
+        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+        .image_sharing_mode(image_sharing_mode)
+        .queue_family_indices(&queue_family_indices)
+        .pre_transform(support.capabilities.current_transform)
+        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+        .present_mode(present_mode)
+        .clipped(true)
+        .old_swapchain(vk::SwapchainKHR::null());
+
+    data.swapchain_format = surface_format.format;
+    data.swapchain_extent = extent;
+    data.swapchain = device.create_swapchain_khr(&info, None)?;
+    data.swapchain_images = device.get_swapchain_images_khr(data.swapchain)?;
+
+    Ok(())
+}}
+
+unsafe fn create_swapchain_image_views(device: &Device, data: &mut AppData) -> Result<()> { unsafe {
+    let components = vk::ComponentMapping::builder()
+        .r(vk::ComponentSwizzle::IDENTITY)
+        .g(vk::ComponentSwizzle::IDENTITY)
+        .b(vk::ComponentSwizzle::IDENTITY)
+        .a(vk::ComponentSwizzle::IDENTITY);
+
+    let subresource_range = vk::ImageSubresourceRange::builder()
+        .aspect_mask(vk::ImageAspectFlags::COLOR)
+        .base_mip_level(0)
+        .level_count(1)
+        .base_array_layer(0)
+        .layer_count(0);
+
+    let generic_info = vk::ImageViewCreateInfo::builder()
+        .view_type(vk::ImageViewType::_2D)
+        .format(data.swapchain_format)
+        .components(components)
+        .subresource_range(subresource_range);
+
+    data.swapchain_image_views = data.swapchain_images.iter()
+        .map(|i| {
+            let image_view_info = generic_info;
+            image_view_info.image(*i);
+            return device.create_image_view(&image_view_info, None).unwrap();
+        })
+        .collect::<Vec<_>>();
+
+    Ok(())
 }}
